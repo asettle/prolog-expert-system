9765e0fc7e6073962c2fb77ea9c16ae97cb9f750
asettle <asettle@suse.com>
2022-05-01 17:16:37 +1000

    Removing extra arg
    Signed-off-by: Alexandra Settle <sgasettl@liverpool.ac.uk>

--- diagnosis.pl
+++ diagnosis.pl
@@ -4,9 +4,9 @@
 diagnosis(D) :- write('Do you know if you been exposed to the virus in the last 14 days?: '),read(E),write('Do you have symptoms?: '),read(SY), diagnosis(D,SY,E).
 
 %Known_Exposure
-diagnosis('Unlikely to be infected',unlikely,SY,E):- SY = 'no' ,(E= 'no'),!.
-diagnosis('Very likely to be infected',likely,SY,E):- SY = yes ,(E= yes),!.
-diagnosis('Possibility of infection',possible,SY,E):- SY = 'no' ,(E= yes),!.
-diagnosis('Possibility of infection',possible,SY,E):- SY = yes ,(E= 'no'),!.
+diagnosis('Unlikely to be infected',SY,E):- SY = 'no' ,(E= 'no'),!.
+diagnosis('Very likely to be infected',SY,E):- SY = yes ,(E= yes),!.
+diagnosis('Possibility of infection',SY,E):- SY = 'no' ,(E= yes),!.
+diagnosis('Possibility of infection',SY,E):- SY = yes ,(E= 'no'),!.
 
 %! is called cut and is used to reduce backtracking
